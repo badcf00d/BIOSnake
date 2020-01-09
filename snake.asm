2c7769bd63cda213aaf559ff89d8b5a8ee645495
Frost <uic31307@contiwan.com>
2020-01-09 15:32:21 +0000

    Working with variables on stack

--- snake.asm
+++ snake.asm
@@ -10,48 +10,58 @@ org 0x7c00
 
 blockWidth: equ 10
 
-    mov si, msg             ; source index to our string
+oldTime: dw 0x0000
+startx: dw 0x0000
+starty: dw 0x0000
+
+
     mov ax, 0x0012          ; 00 = set video mode, 12 = 640x480 16 color graphics
     int 0x10                ; call bios video services
     
-    mov cx, 20              ; x
-    mov dx, 40              ; y
-    push dx
+    mov cx, 20              ; start x
+    mov dx, 40              ; start y
+    push dx                 ; stack is now: start y
     push cx                 ; stack is now: start y, start x
+    jmp drawBlock
 
-drawBlock:
-    mov bx, 0
-    mov ah, 0x0c            ; bios video mode for writing graphics pixels
-    mov al, 9               ; bios color attributes
-    jmp drawBlockLoop
+restoreStartX:
+    sub ax, blockWidth      ; reset to start x
+    push ax                 ; stack is now: start y, start x
+    jmp drawBlock
 
-saveLocationA:
-    sub ax, blockWidth
-    push ax
-    jmp drawBlock           ; stack is now: start y, start x
-
-saveLocationB:
+restoreStartXAndY:
     sub ax, blockWidth      ; reset to start x
     sub bx, blockWidth      ; reset to start y
-    sub cx, blockWidth      ; move back to start of column
-    push bx
-    push ax
-    jmp drawBlock           ; stack is now: start y, start x
+    sub cx, blockWidth      ; reset to start x
+    push bx                 ; stack is now: start y
+    push ax                 ; stack is now: start y, start x
+
+drawBlock:
+    mov bx, 0               ; video page 0
+    mov ah, 0x0c            ; bios video mode for writing graphics pixels
+    mov al, 7               ; bios color attributes, light gray
 
 drawBlockLoop:
-    int 0x10                ; draw
+    int 0x10                ; call bios video services
 
     inc cx                  ; move right
     pop ax                  ; stack is now: start y
-    add ax, blockWidth      ; block width
-    cmp cx, ax              ; end x
-    jne saveLocationA
+    add ax, blockWidth      ; move to end x
+    cmp cx, ax              ; are we done?
+    jne restoreStartX
 
     inc dx                  ; move down, our end x is still in ax
     pop bx                  ; our start y is now in bx
-    add bx, blockWidth      ; block width
-    cmp dx, bx              ; end y
-    jne saveLocationB
+    add bx, blockWidth      ; move to end y
+    cmp dx, bx              ; are we done?
+    jne restoreStartXAndY
+
+delayUntilTick:
+    mov ah,0x00             ; reads system tick counter (~18 Hz) into cx and dx
+    int 0x1a                ; Call real time clock BIOS Services
+    cmp dx, [old_time]       ; Wait for change
+    je in22
+    mov [old_time], dx       ; Save new current time
 
     hlt        
 

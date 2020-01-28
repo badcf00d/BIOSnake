9d91be4ecaddbec841e169e8a0bcd54dce22cf9a
Frost <uic31307@contiwan.com>
2020-01-28 14:52:47 +0000

    Add length check and partition appending

--- snakeDatStick.py
+++ snakeDatStick.py
@@ -1,19 +1,22 @@
 from time import gmtime, strftime
+import os
 
+newFile = open("snake.img", "rb")
+newBootloader = bytearray(newFile.read())
 
+if len(newBootloader) > 440: 
+    print("This image is too big: ", len(newBootloader))
+    raise SystemExit
 
+for i in range(len(newBootloader), 440):
+    newBootloader.append(0)
 
 backupName = "bootsector-"
 backupName += strftime("%d-%m-%Y-%H-%M-%S", gmtime())
 backupName += ".bin"
 
-backup = open(backupName, "ab")
-
-# customize there
-newFile = open("bootsector - 21-01-2020-22-49-40.bin", "rb")
-disk = open(r'\\.\PhysicalDrive3', 'r+b')
-
-
+#backup = open(backupName, "ab")
+#disk = open(r'\\.\PhysicalDrive3', 'r+b')
 
 disk.seek(0)
 oldBootloader = disk.read(512)
@@ -21,12 +24,13 @@ backup.write(oldBootloader)
 print("Old bootloader: \n")
 print(oldBootloader.hex())
 
-newBootloader = newFile.read(512)
+partitionTable = oldBootloader[440:512]
+newBootloader.append(partitionTable)
+
+
 print("New Bootloader: \n")
 print(newBootloader.hex())
 
 disk.seek(0)
-#disk.write(newBootloader)
+disk.write(newBootloader)
 
-#disk.seek(OFFSET * 512)
-#disk.write(data)

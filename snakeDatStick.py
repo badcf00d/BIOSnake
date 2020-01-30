6c8afd6b3cd02e15639f23497024e8c2905b3d0d
Peter <frosticless@hotmail.com>
2020-01-30 23:04:48 +0000

    Done initial Windows implementation

--- snakeDatStick.py
+++ snakeDatStick.py
@@ -4,19 +4,13 @@ import wmi
 import tkinter as tk
 from tkinter import filedialog
 
-c = wmi.WMI()
-for physical_disk in c.Win32_DiskDrive (InterfaceType='USB'):
-      for partition in physical_disk.associators("Win32_DiskDriveToDiskPartition"):
-        for logical_disk in partition.associators("Win32_LogicalDiskToPartition"):
-          print(physical_disk.Caption, physical_disk.DeviceID, logical_disk.Caption)
 
-targetDrive = input("Enter the target physical drive: ")
+input("\nHi,\n\nYou're about to pick the file you want to flash, press enter to continue")
 
 #   Open up a dialog box to pick a file to flash
 root = tk.Tk()
 root.withdraw()
 filePath, fileExtension = os.path.splitext(filedialog.askopenfilename())
-
 
 #   Decide if this is a game image or a backup
 newImageLength = None
@@ -28,16 +22,45 @@ else:
     print("Not sure what file type this is, please use .img for game images, .bak for backups")
     raise SystemExit
 
-
 #   Load the new file
 newFile = open(filePath + fileExtension, "rb")
 newBootloader = bytearray(newFile.read())
-
 
 #   Length check
 if len(newBootloader) > newImageLength: 
     print("Images can't be bigger than %d bytes, this is %d" % (newImageLength, len(newBootloader)))
     raise SystemExit
+else:
+    print("That was: \"%s%s\", %d bytes\n\n" % (filePath, fileExtension, len(newBootloader)))
+
+
+
+input("Now pick the drive you want to flash (probably a USB stick), press enter to continue")
+
+#   Open a dialog box to pick a target drive
+targetDrivePath = filedialog.askdirectory()
+targetDrive = None
+
+for physicalDisk in wmi.WMI().Win32_DiskDrive(InterfaceType='USB'):
+    for partition in physicalDisk.associators("Win32_DiskDriveToDiskPartition"):
+        for logicalDisk in partition.associators("Win32_LogicalDiskToPartition"):
+            #print(physicalDisk.Caption, physicalDisk.DeviceID, logicalDisk.Caption)
+
+            if targetDrivePath[0] == logicalDisk.Caption[0]:
+                confirm = input("You selected \"%s\" is this correct? (y/n)" % physicalDisk.Caption)
+
+                if confirm == "y":
+                    targetDrive = physicalDisk.DeviceID
+                else:
+                    raise SystemExit
+
+if targetDrive == None:
+    print(  "I don't think that was a USB stick, you probably don't want to flash your hard drive with"
+            "this because it probably has your operating system on it, but if you must, change the"
+            "InterfaceType in the python script to HDC or something")
+    raise SystemExit
+
+
 
 #   Pad the file with zeros if it is shorter than we need
 for i in range(len(newBootloader), newImageLength):
